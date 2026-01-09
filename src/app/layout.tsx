3c9c2df848aa7359e12a4698b61c7b1ebbd94f4e
aminefd12 <amine.fadil@ump.ac.ma>
2026-01-09 05:35:13 +0100

    Add Tawk.to chat widget integration
    - Integrate Tawk.to live chat script in root layout
    - Enable customer support chat across all website pages
    - Use dangerouslySetInnerHTML for proper Next.js script implementation
    - Chat widget ID: 696082c93eae05197ecae389/1jegftpbm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/*Start of Tawk.to Script*/}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/696082c93eae05197ecae389/1jegftpbm';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `
+          }}
+        />
+        {/*End of Tawk.to Script*/}
+      </body>
     </html>
   )
 }
